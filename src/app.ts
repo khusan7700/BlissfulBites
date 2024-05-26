b021e3f27cee68f128a9f6f6fddaca724ab0e83e
Khusan <khusan7700@gmail.com>
2024-05-27 01:07:56 +0900

    feat: integrate middleware sessions (46-dars)

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,39 @@
 import express from "express";
 import path from "path";
-import routerAdmin from "./router-admin";
 import router from "./router";
+import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "mySession",
+});
+
 /** 1-ENTRANCE**/
 const app = express();
+// console.log("__dirname:", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
-/** 2-SESSIONS**/
-
+/** 2-SESSIONS**/ // middle war sifatida yozamiz
+app.use(
+  session({
+    secret: "this is a secret",
+    cookie: {
+      maxAge: 1000 * 3600 * 3, // 3h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 /** 3-VIEWS**/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
