84e2f121a968ce6969f163f4570cf93d7eefdc54
Khusan <khusan7700@gmail.com>
2024-05-27 04:59:53 +0900

    feat: upload files into server via multer (47-tamom)

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,16 +1,25 @@
 import express from "express";
-import restaurantController from "./controllers/restaurant.controllar";
 const routerAdmin = express.Router();
+import restaurantController from "./controllers/restaurant.controllar";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 /** Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
 routerAdmin
   .get("/login", restaurantController.getLogin)
   .post("/login", restaurantController.processLogin);
+
+//--------------------------------------------------------------
+
 routerAdmin
   .get("/signup", restaurantController.getSignup)
-  .post("/signup", restaurantController.processSignup);
+  .post(
+    "/signup",
+    makeUploader("members").single("memberImage"),
+    restaurantController.processSignup
+  );
+//--------------------------------------------------------------
 
 routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
@@ -18,12 +27,22 @@ routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 /** Product */
 
 routerAdmin.get(
-  "/products/all",
+  "/product/all",
   restaurantController.verifyRestaurant, // middle wayda murojat qilyapmiz
   productController.getAllProducts
 );
-routerAdmin.post("/product/create", productController.createNewProduct);
-routerAdmin.post("/product/:id", productController.updateChosenProduct);
+
+routerAdmin.post(
+  "/product/create",
+  restaurantController.verifyRestaurant,
+  makeUploader("products").array("productImages", 5),
+  productController.createNewProduct
+);
+routerAdmin.post(
+  "/product/:id",
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProduct
+);
 
 /** User */
 
