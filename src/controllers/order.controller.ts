d0f9959dfa2a6d9e651255721def3fa057d9e749
Khusan <khusan7700@gmail.com>
2024-06-28 01:11:04 +0900

    feat: develop getMyorders rest API (76 end)

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,56 @@
+import { ExtendedRequest } from "../libs/types/member";
+import { T } from "../libs/types/common";
+import { Response } from "express";
+import Errors, { HttpCode } from "../libs/Errors";
+import OrderService from "../models/Order.service";
+import { OrderStatus } from "../libs/enums/order.enum";
+import { OrderInquiry } from "../libs/types/order";
+
+const orderService = new OrderService();
+
+const orderController: T = {};
+
+//-------------------------------create order----------------------
+//-------------------------------create order----------------------
+//-------------------------------create order----------------------
+//-------------------------------create order----------------------
+//-------------------------------create order----------------------
+orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("createOrder");
+    const result = await orderService.createOrder(req.member, req.body);
+
+    res.status(HttpCode.CREATED).json(result);
+  } catch (err) {
+    console.log("Error, createOrder:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+//-------------------------------create order----------------------
+//-------------------------------create order----------------------
+//-------------------------------create order----------------------
+//-------------------------------create order----------------------
+//-------------------------------create order----------------------
+
+orderController.getMyOrders = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("getMyOrders");
+    const { page, limit, orderStatus } = req.query;
+    const inquiry: OrderInquiry = {
+      page: Number(page),
+      limit: Number(limit),
+      orderStatus: orderStatus as OrderStatus,
+    };
+    console.log("inquiry:", inquiry);
+    const result = await orderService.getMyOrders(req.member, inquiry);
+
+    res.status(HttpCode.CREATED).json(result);
+  } catch (err) {
+    console.log("Error, getMyOrders:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+export default orderController;
---
+++ src/models/Order.service.ts
@@ -0,0 +1,112 @@
+import OrderItemModel from "../schema/OrderItem.model";
+import OrderModel from "../schema/Order.model";
+import { Member } from "../libs/types/member";
+import { Order, OrderInquiry, OrderItemInput } from "../libs/types/order";
+import { shapeIntoMongooseObjectId } from "../libs/config";
+import Errors, { Message } from "../libs/Errors";
+import { HttpCode } from "../libs/Errors";
+import { ObjectId } from "mongoose";
+import { exec } from "child_process";
+
+class OrderService {
+  private readonly orderModel;
+  private readonly orderItemModel;
+
+  constructor() {
+    this.orderModel = OrderModel;
+    this.orderItemModel = OrderItemModel;
+  }
+  //-------------------------------create order----------------------
+  //-------------------------------create order----------------------
+  //-------------------------------create order----------------------
+  //-------------------------------create order----------------------
+
+  public async createOrder(
+    member: Member,
+    input: OrderItemInput[]
+  ): Promise<Order> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const amount = input.reduce((accumulator: number, item: OrderItemInput) => {
+      return accumulator + item.itemPrice * item.itemQuantity;
+    }, 0);
+    const delivery = amount < 100 ? 5 : 0;
+
+    try {
+      const newOrder: Order = await this.orderModel.create({
+        orderTotal: amount + delivery,
+        orderDelivery: delivery,
+        memberId: memberId,
+      });
+      console.log("orderid:", newOrder._id);
+      const orderId = newOrder._id;
+      await this.recordOrderItem(orderId, input);
+      return newOrder;
+    } catch (err) {
+      console.log("Error, model: createOrder:", err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
+  }
+  //-------------------------------record Order Item----------------------
+  //-------------------------------record Order Item----------------------
+  //-------------------------------record Order Item----------------------
+  //-------------------------------record Order Item----------------------
+
+  private async recordOrderItem(
+    orderId: ObjectId,
+    input: OrderItemInput[]
+  ): Promise<void> {
+    const promisedList = input.map(async (item: OrderItemInput) => {
+      item.orderId = orderId;
+      item.productId = shapeIntoMongooseObjectId(item.productId);
+      await this.orderItemModel.create(item);
+      return "INTERTED";
+    });
+    console.log("promiselist----->", promisedList);
+    const orderItemsState = await Promise.all(promisedList);
+    console.log("orderItemStatus:", orderItemsState);
+  }
+  //-------------------------------create order----------------------
+  //-------------------------------create order----------------------
+  //-------------------------------create order----------------------
+  //-------------------------------create order----------------------
+
+  public async getMyOrders(
+    member: Member,
+    inquiry: OrderInquiry
+  ): Promise<Order[]> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const matches = { memberId: memberId, orderStatus: inquiry.orderStatus };
+
+    const result = await this.orderModel
+      .aggregate([
+        { $match: matches }, //type modellar
+        { $sort: { updateAt: -1 } },
+        { $skip: (inquiry.page - 1) * inquiry.limit },
+        { $limit: inquiry.limit },
+        {
+          $lookup: {
+            from: "orderItems",
+            localField: "_id",
+            foreignField: "orderId",
+            as: "orderItems",
+          },
+        },
+        {
+          $lookup: {
+            from: "products",
+            localField: "orderItems.productId",
+            foreignField: "_id",
+            as: "productData",
+          },
+        },
+      ])
+
+      .exec();
+    return result;
+
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
+  }
+}
+
+export default OrderService;
