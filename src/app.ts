101b16d44422b4daf9f1e817d45d9b29ef72aa19
Khusan <khusan7700@gmail.com>
2025-07-14 02:12:58 +0900

    feat: developed EJS mobile view

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,10 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
-
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
 import { T } from "./libs/types/common";
 import cookieParser from "cookie-parser";
-
 import { Server as SocketIOServer } from "socket.io";
 import http from "http";
 
